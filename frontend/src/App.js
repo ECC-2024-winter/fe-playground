de592c4cdd94bd971380a628f1d8b07ad9a2fc81
hslee <hanseul.dev@gmail.com>
2025-01-23 16:19:01 +0900

    feat: fe-playground 초기 화면 구현

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const teams = [
+    {
+      name: "1팀❤️",
+      members: ["나는 연0", "나는 소0", "나는 수0", "나는 채0"],
+    },
+    { name: "2팀💛", members: ["나는 정민", "나는 소0", "나는 준0"] },
+    { name: "3팀💚", members: ["나는 서0", "나는 다0"] },
+    { name: "4팀💖", members: ["나는 현0", "나는 아0"] },
+  ];
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>FE Playground에 오신 걸 환영합니다!</h1>
+        <div>
+          {teams.map((team, index) => (
+            <ul key={index}>
+              {team.name}
+              {team.members.map((member, memberIndex) => (
+                <li key={memberIndex}>{member}</li>
+              ))}
+            </ul>
+          ))}
+        </div>
       </header>
     </div>
   );
